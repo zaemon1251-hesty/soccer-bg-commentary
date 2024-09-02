3606579cd65eeadfc2806f478897eab3e1951db2
zaemon1251-hesty <hisakawa1251@gmail.com>
2024-09-02 15:41:24 +0900

    add  impl addinfo_retrieval with langchain

--- src/sn_providing/addinfo_retrieval.py
+++ src/sn_providing/addinfo_retrieval.py
@@ -4,24 +4,40 @@ from datetime import datetime
 from functools import partial
 import logging
 
+from pathlib import Path
 from tap import Tap
 from loguru import logger
+from dotenv import load_dotenv
+
 from llama_index.core import ( 
     SimpleKeywordTableIndex, 
     SimpleDirectoryReader, 
     StorageContext, 
     load_index_from_storage 
 )
-from llama_index.llms.openai import OpenAI
+from llama_index.llms.openai import OpenAI as LLamaIndexOpenAI
+
+from langchain_openai import ChatOpenAI as LangChainOpenAI
+from langchain_core.documents import (
+    Document,
+)
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.retrievers import TFIDFRetriever
+from langchain_core.prompts import PromptTemplate
 
 from sn_providing.construct_query import SpottingDataList
 
+#　(project-root)/.env を読み込む
+load_dotenv()
 
 logger.add(
     "logs/{}.log".format(datetime.now().strftime("%Y-%m-%d-%H-%M-%S")),
     level="DEBUG",
 )
-# llama_index のログを標準出力に出す
+# llama_index,langchain のログを標準出力に出す
+# TODO loguru.logger に統一したい
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
@@ -38,13 +54,6 @@ MODEL_CONFIG = {
     "temperature": 0,
 }
 
-PROMPT_TEMPLATE = \
-"""
-{instruction}
-===
-{query}
-"""
-
 INSTRUCTION = \
 """
 Based on Wikipedia, provide interesting events (records, stats) related to the current context query.
@@ -52,33 +61,41 @@ Select at least 5 events at random.
 Write each description in a brief one-sentence format. Do not provide any other message.
 """
 
+# 知識ベースのデータ保存場所
+DOCUMENT_DIR = Path("./data/addinfo_retrieval")
+
 # llama indexのデータ構造保存場所
-DOCUMENT_DIR = "./data/addinfo_retrieval"
-PERSIST_DIR = "./storage/addinfo_retrieval"
+PERSIST_LLAMAINDEX_DIR = Path("./storage/llama_index")
+
+# langchainのデータ構造保存場所
+PERSIST_LANGCHAIN_DIR = Path("./storage/langchain")
 
 
-if __name__ == "__main__":
-    args = Arguments().parse_args()
-
-    format_prompt_with_query = partial(PROMPT_TEMPLATE.format, instruction=INSTRUCTION)
-
-    spotting_data_list = SpottingDataList.from_jsonline(args.input_file)
-
+def run_llamaindex(spotting_data_list: SpottingDataList, output_file: str):
+    prompt_template = \
+    """
+    {instruction}
+    === query context
+    {query}
+    """
     # build index
     # check if the index is already stored
-    if not os.path.exists(PERSIST_DIR):
+    if not os.path.exists(PERSIST_LLAMAINDEX_DIR):
         # store mode
         documents = SimpleDirectoryReader(DOCUMENT_DIR).load_data()
         # SimpleKeywordTableIndex は 単語の正規表現 に基づく
         index = SimpleKeywordTableIndex.from_documents(documents)
         # store to disk
-        index.storage_context.persist(persist_dir=PERSIST_DIR)
+        index.storage_context.persist(persist_dir=PERSIST_LLAMAINDEX_DIR)
     else:
         # load
-        storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+        storage_context = StorageContext.from_defaults(persist_dir=PERSIST_LLAMAINDEX_DIR)
         index = load_index_from_storage(storage_context)
 
-    llm = OpenAI(
+    # 毎回instructionは同じなので、先に入力しておく
+    format_prompt_with_query = partial(prompt_template.format, instruction=INSTRUCTION)
+
+    llm = LLamaIndexOpenAI(
         model=MODEL_CONFIG["model_name"],
         temperature=MODEL_CONFIG["temperature"],
     )
@@ -97,6 +114,80 @@ if __name__ == "__main__":
         result_list.spottings.append(spotting_data)
         
         logger.info(f"Response: {response}")
-
     # save
-    result_list.to_jsonline(args.output_file)
+    result_list.to_jsonline(output_file)
+
+
+def run_langchain(spotting_data_list: SpottingDataList, output_file: str):
+    def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs)
+    
+    prompt_template = \
+    """
+    {instruction}
+    
+    === wikipedia documents
+    {documents}
+    
+    === query context
+    {query}
+    
+    Answer:
+    """
+
+    # インデックスの構築
+    if not os.path.exists(PERSIST_LANGCHAIN_DIR):
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        documents = []
+        for doc_path in os.listdir(DOCUMENT_DIR):
+            with open(os.path.join(DOCUMENT_DIR, doc_path), "r") as f:
+                doc = Document(page_content=f.read())
+                documents.append(doc)
+        splits = text_splitter.split_documents(documents)
+        retriever = TFIDFRetriever.from_documents(splits)
+        # 保存
+        retriever.save_local(folder_path=PERSIST_LANGCHAIN_DIR)
+    else:
+        # ローカルから読み込み
+        retriever = TFIDFRetriever.load_local(
+            folder_path=PERSIST_LANGCHAIN_DIR, 
+            allow_dangerous_deserialization=True
+        )
+
+    llm = LangChainOpenAI(
+        model=MODEL_CONFIG["model_name"],
+        temperature=MODEL_CONFIG["temperature"],
+    )
+    
+    prompt = PromptTemplate.from_template(prompt_template)
+    
+    rag_chain = (
+        {"instruction": lambda _: INSTRUCTION, "documents": retriever | format_docs, "query": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+    
+    result_list = SpottingDataList([])
+    for spotting_data in spotting_data_list.spottings[:10]: # run only 10 head for debug
+        logger.info(f"Querying: {spotting_data.query}")
+        if spotting_data.query is None:
+            continue
+        
+        response = rag_chain.invoke(spotting_data.query)
+        spotting_data.addiofo = response
+        result_list.spottings.append(spotting_data)
+        
+        logger.info(f"Response: {response}")
+    # save
+    result_list.to_jsonline(output_file)
+
+
+if __name__ == "__main__":
+    args = Arguments().parse_args()
+
+    spotting_data_list = SpottingDataList.from_jsonline(args.input_file)
+    
+    run_langchain(spotting_data_list, args.output_file)
+
+
--- src/sn_providing/langchain_example.py
+++ src/sn_providing/langchain_example.py
@@ -9,11 +9,25 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.prompts import PromptTemplate
 
 from langchain_openai import ChatOpenAI
 
 if os.environ.get("OPENAI_API_KEY", "") == "":
     os.environ["OPENAI_API_KEY"] = getpass.getpass()
+
+
+template = """
+Use the following pieces of context to answer the question at the end.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+Use three sentences maximum and keep the answer as concise as possible.
+Always say "thanks for asking!" at the end of the answer.
+
+{context}
+
+Question: {question}
+
+Helpful Answer:"""
 
 
 llm = ChatOpenAI(model="gpt-4o-mini")
@@ -36,7 +50,7 @@ vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings
 
 # Retrieve and generate using the relevant snippets of the blog.
 retriever = vectorstore.as_retriever()
-prompt = hub.pull("rlm/rag-prompt")
+prompt = PromptTemplate.from_template(template) # Input variables: {"context": "filler context", "question": "filler question"}
 
 
 def format_docs(docs):
