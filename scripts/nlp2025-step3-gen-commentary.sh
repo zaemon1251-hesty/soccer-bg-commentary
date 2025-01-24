f0eddd243473179e4eae5081f1468197a6fbedb7
zaemon1251-hesty <hisakawa1251@gmail.com>
2025-01-25 00:24:29 +0900

    update

--- scripts/nlp2025-step3-gen-commentary.sh
+++ scripts/nlp2025-step3-gen-commentary.sh
@@ -13,58 +13,36 @@
 
 model_name=gpt-4o
 
-# A: ドキュメント検索せず、LLMに任せる
-INPUT_FILE="outputs/step2/evaluation-target-sn-gamestate.jsonl"  # 選手同定モジュールの出力を入れる
-OUTPUT_A_FILE="outputs/step3/$model_name/evaluation-target-a.jsonl"
-# uv run python src/sn_providing/addinfo_retrieval.py \
-#     --input_file "$INPUT_FILE" \
-#     --output_file "$OUTPUT_A_FILE" \
-#     --retriever_type "openai-embedding" \
-#     --no_retrieval
-
-
-# B: システム全体を動かす
-INPUT_FILE="outputs/step2/evaluation-target-sn-gamestate.jsonl"
-OUTPUT_B_FILE="outputs/step3/$model_name/evaluation-target-b.jsonl"
-# uv run python src/sn_providing/addinfo_retrieval.py \
-#     --input_file "$INPUT_FILE" \
-#     --output_file "$OUTPUT_B_FILE" \
-#     --retriever_type "openai-embedding"
-
-
-# C: 正解の選手リスト、正解の文書を選ぶ
-INPUT_FILE="outputs/step2/evaluation-target-correct-player-list.jsonl"
-OUTPUT_C_FILE="outputs/step3/$model_name/evaluation-target-c.jsonl"
-uv run python src/sn_providing/addinfo_retrieval.py \
-    --input_file "$INPUT_FILE" \
-    --output_file "$OUTPUT_C_FILE" \
-    --retriever_type "openai-embedding" \
-    --reference_documents_yaml "data/reference_documents/evaluation-samples.yaml"
-
 
 # Astar: ドキュメント検索せず、LLMに任せる + アクション情報を追加
 INPUT_FILE="outputs/step2/evaluation-target-sn-gamestate-w-action.jsonl"  # 選手同定モジュールの出力を入れる
 OUTPUT_Astar_FILE="outputs/step3/$model_name/evaluation-target-a-star.jsonl"
-# uv run python src/sn_providing/addinfo_retrieval.py \
-#     --input_file "$INPUT_FILE" \
-#     --output_file "$OUTPUT_Astar_FILE" \
-#     --retriever_type "openai-embedding" \
-#     --no_retrieval
+echo "Start Baseline"
+uv run python src/sn_providing/addinfo_retrieval.py \
+    --model $model_name \
+    --input_file "$INPUT_FILE" \
+    --output_file "$OUTPUT_Astar_FILE" \
+    --retriever_type "openai-embedding" \
+    --no_retrieval
 
 
 # Bstar: システム全体を動かす + アクション情報を追加
 INPUT_FILE="outputs/step2/evaluation-target-sn-gamestate-w-action.jsonl"
 OUTPUT_Bstar_FILE="outputs/step3/$model_name/evaluation-target-b-star.jsonl"
-# uv run python src/sn_providing/addinfo_retrieval.py \
-#     --input_file "$INPUT_FILE" \
-#     --output_file "$OUTPUT_Bstar_FILE" \
-#     --retriever_type "openai-embedding"
+echo "Start System"
+uv run python src/sn_providing/addinfo_retrieval.py \
+    --model $model_name \
+    --input_file "$INPUT_FILE" \
+    --output_file "$OUTPUT_Bstar_FILE" \
+    --retriever_type "openai-embedding"
 
 
 # Cstar: 正解の選手リスト、正解の文書を選ぶ + アクション情報を追加
 INPUT_FILE="outputs/step2/evaluation-target-correct-player-list-w-action.jsonl"
 OUTPUT_Cstar_FILE="outputs/step3/$model_name/evaluation-target-c-star.jsonl"
+echo "Start System + GD + GP"
 uv run python src/sn_providing/addinfo_retrieval.py \
+    --model $model_name \
     --input_file "$INPUT_FILE" \
     --output_file "$OUTPUT_Cstar_FILE" \
     --retriever_type "openai-embedding" \
@@ -74,7 +52,9 @@ uv run python src/sn_providing/addinfo_retrieval.py \
 # Bsharp: 正解の選手リスト + 文書抽出 + アクション情報を追加
 INPUT_FILE="outputs/step2/evaluation-target-correct-player-list-w-action.jsonl"
 OUTPUT_Bsharp_FILE="outputs/step3/$model_name/evaluation-target-b-sharp.jsonl"
+echo "Start System + GP"
 uv run python src/sn_providing/addinfo_retrieval.py \
+    --model $model_name \
     --input_file "$INPUT_FILE" \
     --output_file "$OUTPUT_Bsharp_FILE" \
     --retriever_type "openai-embedding" \
--- src/sn_providing/addinfo_retrieval.py
+++ src/sn_providing/addinfo_retrieval.py
@@ -22,24 +22,30 @@ from langchain_openai.embeddings import OpenAIEmbeddings
 
 from sn_providing.entity import SpottingDataList, ReferenceDoc, RetrieverType
 
-#　(project-root)/.env を読み込む
+# (project-root)/.env を読み込む
 load_dotenv()
 
 # llama_index,langchain のログを標準出力に出す
 logging.basicConfig(
-    level=logging.DEBUG, 
-    filename="logs/{}.log".format(datetime.now().strftime("%Y-%m-%d-%H-%M-%S"))
+    level=logging.DEBUG,
+    filename="logs/{}.log".format(datetime.now().strftime("%Y-%m-%d-%H-%M-%S")),
 )
 
 
-
 class Arguments(Tap):
-    game: str | None = None # useless
+    game: str | None = None  # useless
     input_file: str
     output_file: str
     retriever_type: RetrieverType = "tfidf"
     no_retrieval: bool = False
     reference_documents_yaml: str | None = None
+    model: str = "gpt-4o"
+    temperature: float = 0
+    embedding_model: str = "text-embedding-ada-002"
+    chunk_size: int = 1000
+    search_k: int = 10
+    search_score_threshold: float = 0.7
+
 
 # constants
 MODEL_CONFIG = {
@@ -57,16 +63,14 @@ SEARCH_CONFIG = {
     "score_threshold": 0.7,
 }
 
-INSTRUCTION = \
-"""You are a professional color commentator for a live broadcast of soccer. 
+INSTRUCTION = """You are a professional color commentator for a live broadcast of soccer. 
 Using the documents below, 
 provide just one comment with a fact, such as player records or team statistics, relevant to the current soccer match. 
 The comment should be short, clear, accurate, and suitable for live commentary. 
 The game date will be given as YYYY-MM-DD. Do not use information dated after this.
 This comment should be natural comments following the previous comments given to the prompt."""
 # No retrievalの場合のプロンプト
-prompt_template_no_retrieval = \
-"""{instruction}
+prompt_template_no_retrieval = """{instruction}
 
 ===
 {query}
@@ -74,8 +78,7 @@ prompt_template_no_retrieval = \
 Comment:"""
 
 # documentが与えられる場合のプロンプト
-prompt_template = \
-"""{instruction}
+prompt_template = """{instruction}
 
 ===documents
 {documents}
@@ -91,12 +94,15 @@ DOCUMENT_DIR = Path("./data/addinfo_retrieval")
 PERSIST_LANGCHAIN_DIR = Path("./storage/langchain-embedding-ada002")
 
 
-
 def run_langchain(
-    spotting_data_list: SpottingDataList, 
-    output_file: str, retriever_type: RetrieverType,
+    spotting_data_list: SpottingDataList,
+    output_file: str,
+    retriever_type: RetrieverType,
     no_retrieval: bool = False,
-    reference_documents_yaml: str | None = None
+    reference_documents_yaml: str | None = None,
+    model_config: dict = MODEL_CONFIG,
+    embedding_config: dict = EMBEDDING_CONFIG,
+    search_config: dict = SEARCH_CONFIG,
 ):
     """
     LangChainを使って付加的情報を生成する
@@ -114,11 +120,11 @@ def run_langchain(
         logging.info(f"Overall Prompt: {prompt}")
         return prompt
 
-    retriever = get_retriever_langchain(retriever_type, langchain_store_dir=PERSIST_LANGCHAIN_DIR)
-
-    llm = LangChainOpenAI(
-        **MODEL_CONFIG
+    retriever = get_retriever_langchain(
+        retriever_type, langchain_store_dir=PERSIST_LANGCHAIN_DIR
     )
+
+    llm = LangChainOpenAI(**MODEL_CONFIG)
 
     # リファレンスドキュメントが与えられた場合使う
     reference_doc_data = None
@@ -128,8 +134,9 @@ def run_langchain(
     if no_retrieval:
         rag_chain = (
             {
-                "instruction": lambda _: INSTRUCTION,  
-                "query": lambda spotting_data: spotting_data.query}
+                "instruction": lambda _: INSTRUCTION,
+                "query": lambda spotting_data: spotting_data.query,
+            }
             | PromptTemplate.from_template(prompt_template_no_retrieval)
             | log_prompt
             | llm
@@ -138,28 +145,36 @@ def run_langchain(
     elif reference_documents_yaml is not None:
         # 正解文書の準備
         reference_doc_data = ReferenceDoc.get_list_from_yaml(reference_documents_yaml)
-        get_reference_documents_partial = partial(ReferenceDoc.get_reference_documents, reference_documents=reference_doc_data)
+        get_reference_documents_partial = partial(
+            ReferenceDoc.get_reference_documents, reference_documents=reference_doc_data
+        )
 
         rag_chain = (
             {
-                "instruction": lambda _: INSTRUCTION, 
-                "documents": lambda spotting_data: get_reference_documents_partial(spotting_data.game, spotting_data.half, spotting_data.game_time),
-                "query": lambda spotting_data: spotting_data.query}
+                "instruction": lambda _: INSTRUCTION,
+                "documents": lambda spotting_data: get_reference_documents_partial(
+                    spotting_data.game, spotting_data.half, spotting_data.game_time
+                ),
+                "query": lambda spotting_data: spotting_data.query,
+            }
             | PromptTemplate.from_template(prompt_template)
             | log_prompt
             | llm
             | StrOutputParser()
         )
     else:
+
         def process_docs(spotting_data):
-            query = lambda _: spotting_data.query # noqa
+            query = lambda _: spotting_data.query  # noqa
             docs = query | retriever | log_documents | format_docs
             return docs
+
         rag_chain = (
             {
-                "instruction": lambda _: INSTRUCTION, 
-                "documents": lambda spotting_data: process_docs(spotting_data), 
-                "query": lambda spotting_data: spotting_data.query}
+                "instruction": lambda _: INSTRUCTION,
+                "documents": lambda spotting_data: process_docs(spotting_data),
+                "query": lambda spotting_data: spotting_data.query,
+            }
             | PromptTemplate.from_template(prompt_template)
             | log_prompt
             | llm
@@ -172,25 +187,31 @@ def run_langchain(
         logging.info(f"Query: {spotting_data.query}")
         if spotting_data.query is None:
             continue
-        
-        if reference_doc_data is not None and \
-            get_reference_documents_partial(spotting_data.game, spotting_data.half, spotting_data.game_time) is None:
+
+        if (
+            reference_doc_data is not None
+            and get_reference_documents_partial(
+                spotting_data.game, spotting_data.half, spotting_data.game_time
+            )
+            is None
+        ):
             # 正解文書がない場合はスキップ
-            logging.info(f"skip : {spotting_data.game}, {spotting_data.half}, {spotting_data.game_time}")
+            logging.info(
+                f"skip : {spotting_data.game}, {spotting_data.half}, {spotting_data.game_time}"
+            )
             continue
-        
+
         response = rag_chain.invoke(spotting_data)
         spotting_data.generated_text = response
         result_list.spottings.append(spotting_data)
-        
+
         logging.info(f"Response: {response}")
     # save
     result_list.to_jsonline(output_file)
 
 
 def get_retriever_langchain(
-    type: RetrieverType, 
-    langchain_store_dir: Path
+    type: RetrieverType, langchain_store_dir: Path
 ) -> BaseRetriever:
     if type == "tfidf":
         if not os.path.exists(langchain_store_dir):
@@ -202,8 +223,7 @@ def get_retriever_langchain(
         else:
             # ローカルから読み込み
             retriever = TFIDFRetriever.load_local(
-                folder_path=langchain_store_dir, 
-                allow_dangerous_deserialization=True
+                folder_path=langchain_store_dir, allow_dangerous_deserialization=True
             )
         retriever.k = SEARCH_CONFIG["k"]
         return retriever
@@ -212,32 +232,40 @@ def get_retriever_langchain(
         if not os.path.exists(langchain_store_dir):
             # インデックスの構築
             splits = get_document_splits(DOCUMENT_DIR)
-            
+
             vectorstore = FAISS.from_documents(documents=splits, embedding=embeddings)
             retriever = vectorstore.as_retriever(
                 search_type="similarity_score_threshold",
-                search_kwargs={'score_threshold': EMBEDDING_CONFIG["score_threshold"], 'k': EMBEDDING_CONFIG["k"]}
+                search_kwargs={
+                    "score_threshold": EMBEDDING_CONFIG["score_threshold"],
+                    "k": EMBEDDING_CONFIG["k"],
+                },
             )
             # 保存
             vectorstore.save_local(folder_path=langchain_store_dir)
         else:
             # ローカルから読み込み
             vectorstore: FAISS = FAISS.load_local(
-                folder_path=langchain_store_dir, 
+                folder_path=langchain_store_dir,
                 embeddings=embeddings,
-                allow_dangerous_deserialization=True
+                allow_dangerous_deserialization=True,
             )
             retriever = vectorstore.as_retriever(
-                search_type="similarity_score_threshold",
-                search_kwargs=SEARCH_CONFIG
+                search_type="similarity_score_threshold", search_kwargs=SEARCH_CONFIG
             )
         return retriever
     else:
-        raise ValueError(f"Invalid retriever type: {type}. Use 'tfidf' or 'openai-embedding'.")
+        raise ValueError(
+            f"Invalid retriever type: {type}. Use 'tfidf' or 'openai-embedding'."
+        )
 
 
-def get_document_splits(ducument_dir: Path, chunk_size: int = 1000, chunk_overlap: int = 100):
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+def get_document_splits(
+    ducument_dir: Path, chunk_size: int = 1000, chunk_overlap: int = 100
+):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size, chunk_overlap=chunk_overlap
+    )
     documents = []
     for doc_path in os.listdir(ducument_dir):
         with open(os.path.join(ducument_dir, doc_path), "r") as f:
@@ -251,14 +279,42 @@ if __name__ == "__main__":
     args = Arguments().parse_args()
 
     spotting_data_list = SpottingDataList.from_jsonline(args.input_file)
+    logging.info("Arguments:")
+    logging.info(f"Game: {args.game}")
+    logging.info(f"Input File: {args.input_file}")
+    logging.info(f"Output File: {args.output_file}")
+    logging.info(f"Retriever Type: {args.retriever_type}")
+    logging.info(f"No Retrieval: {args.no_retrieval}")
+    logging.info(f"Reference Documents: {args.reference_documents_yaml}")
+
+    model_config = {
+        "model": args.model,
+        "temperature": args.temperature,
+    }
+    embedding_config = {
+        "model": args.embedding_model,
+        "chunk_size": args.chunk_size,
+    }
+
+    search_config = {
+        "k": args.search_k,
+        "score_threshold": args.search_score_threshold,
+    }
+
+    logging.info(f"Model Config: {model_config}")
+    logging.info(f"Embedding Config: {embedding_config}")
+    logging.info(f"Search Config: {search_config}")
 
     if args.no_retrieval:
         INSTRUCTION = INSTRUCTION.replace("Using the documents below,", "")
 
     run_langchain(
-        spotting_data_list, 
-        args.output_file, 
-        args.retriever_type, 
-        args.no_retrieval, 
-        args.reference_documents_yaml
+        spotting_data_list,
+        args.output_file,
+        args.retriever_type,
+        args.no_retrieval,
+        args.reference_documents_yaml,
+        model_config,
+        embedding_config,
+        search_config,
     )
